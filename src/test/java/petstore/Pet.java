38040235a4c3c994100a4b4f61c687830a9927e2
rodrigo.caetano <rodrigo.caetano@hypeflame.com.br>
2021-09-07 17:06:36 -0300

    criado primeiro metodo para validar o retorno da api usando um body.json externo e criando um metodo para ler este arquivo.json

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,38 @@
+package petstore;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2/pet";
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    @Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
+
+
+}
